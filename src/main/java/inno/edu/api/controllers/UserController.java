e948bf47c6980be824ebc53eb5cbe984f493b4f2
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-11-06 21:41:40 -0800

    Introduce the user controller.

---
+++ src/main/java/inno/edu/api/controllers/UserController.java
@@ -0,0 +1,14 @@
+package inno.edu.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @GetMapping
+    public String users() {
+        return "Greetings from Spring Boot!";
+    }
+}
