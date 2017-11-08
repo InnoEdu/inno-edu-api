45811618ac8c59b8a3a887e06e0746b435b5af64
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-11-07 23:29:07 -0800

    Add user management integration test.

--- src/test-integration/java/inno/edu/api/features/UserManagement.java
+++ src/test-integration/java/inno/edu/api/features/UserManagement.java
@@ -1,14 +1,30 @@
 package inno.edu.api.features;
 
+import inno.edu.api.IntegrationTest;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class UserManagement {
+public class UserManagement extends IntegrationTest {
+    @Test
+    public void shouldListUsers() throws Exception {
+        this.mockMvc.perform(get("/api/users")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._embedded.userResourceList[0].id", is(1)))
+                .andExpect(jsonPath("$._embedded.userResourceList[0].firstName", is("Dave")))
+                .andExpect(jsonPath("$._embedded.userResourceList[0].lastName", is("Syer")));
+    }
 
     @Test
-    public void dummyTest() {
-        assertThat("Hello World!", is("Hello World!"));
+    public void shouldGetUserById() throws Exception {
+        this.mockMvc.perform(get("/api/users/1")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.firstName", is("Dave")))
+                .andExpect(jsonPath("$.lastName", is("Syer")));
     }
 }
