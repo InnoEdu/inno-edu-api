87d5f8a3636d4fe591803a61da93941b0c063286
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-11-10 23:17:28 -0800

    Add links to the user resource.

--- src/main/java/inno/edu/api/controllers/resources/UserResource.java
+++ src/main/java/inno/edu/api/controllers/resources/UserResource.java
@@ -1,6 +1,7 @@
 package inno.edu.api.controllers.resources;
 
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import inno.edu.api.controllers.AppointmentController;
 import inno.edu.api.controllers.AvailabilityController;
 import inno.edu.api.controllers.UserController;
 import inno.edu.api.domain.user.models.User;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
+import static inno.edu.api.domain.appointment.models.AppointmentStatus.PROPOSED;
 import static org.springframework.hateoas.core.DummyInvocationUtils.methodOn;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
@@ -24,6 +26,8 @@ public class UserResource extends ResourceSupport {
         this.user = user;
         add(linkTo(methodOn(UserController.class).get(user.getId())).withSelfRel());
         add(linkTo(methodOn(AvailabilityController.class).allByUser(user.getId())).withRel("availability"));
+        add(linkTo(methodOn(AppointmentController.class).allByMentor(user.getId(), PROPOSED)).withRel("mentor-proposed-appointments"));
+        add(linkTo(methodOn(AppointmentController.class).allByMentee(user.getId(), PROPOSED)).withRel("mentee-proposed-appointments"));
     }
 
     public ResponseEntity<?> toCreated() {
