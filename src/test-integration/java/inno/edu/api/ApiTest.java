a95bd2ad1ff58a167a35222c4e6fb032130b28f3
gustavodido <gustavo_di_domenico@gap.com>
2017-11-14 16:13:59 -0800

    Add the spring pre-prod profiles.

--- src/test-integration/java/inno/edu/api/ApiTest.java
+++ src/test-integration/java/inno/edu/api/ApiTest.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -11,6 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:db/SetUpDatabase.sql")
 public abstract class ApiTest {
 
