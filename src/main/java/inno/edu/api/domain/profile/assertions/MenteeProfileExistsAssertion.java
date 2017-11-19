3aca37b39d35d007d0afa97c369cd76ddb883ba1
gustavodido <gustavo_di_domenico@gap.com>
2017-11-19 14:03:42 -0800

    Add mentee profile exist assertion.

---
+++ src/main/java/inno/edu/api/domain/profile/assertions/MenteeProfileExistsAssertion.java
@@ -0,0 +1,22 @@
+package inno.edu.api.domain.profile.assertions;
+
+import inno.edu.api.domain.profile.exceptions.ProfileNotFoundException;
+import inno.edu.api.domain.profile.repositories.MenteeProfileRepository;
+import inno.edu.api.infrastructure.annotations.Assertion;
+
+import java.util.UUID;
+
+@Assertion
+public class MenteeProfileExistsAssertion {
+    private final MenteeProfileRepository profileRepository;
+
+    public MenteeProfileExistsAssertion(MenteeProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
+
+    public void run(UUID id) {
+        if (!profileRepository.exists(id)) {
+                throw new ProfileNotFoundException(id);
+        }
+    }
+}
--- src/test/java/inno/edu/api/domain/profile/assertions/MenteeProfileExistsAssertionTest.java
+++ src/test/java/inno/edu/api/domain/profile/assertions/MenteeProfileExistsAssertionTest.java
@@ -1,7 +1,7 @@
 package inno.edu.api.domain.profile.assertions;
 
 import inno.edu.api.domain.profile.exceptions.ProfileNotFoundException;
-import inno.edu.api.domain.profile.repositories.MentorProfileRepository;
+import inno.edu.api.domain.profile.repositories.MenteeProfileRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -12,22 +12,22 @@ import static inno.edu.api.support.ProfileFactory.feiProfile;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class MentorProfileExistsAssertionTest {
+public class MenteeProfileExistsAssertionTest {
     @Mock
-    private MentorProfileRepository profileRepository;
+    private MenteeProfileRepository profileRepository;
 
     @InjectMocks
-    private MentorProfileExistsAssertion profileExistsAssertion;
+    private MenteeProfileExistsAssertion profileExistsAssertion;
 
     @Test(expected = ProfileNotFoundException.class)
-    public void shouldThrowExceptionIfMentorProfileDoesNotExist() {
+    public void shouldThrowExceptionIfMenteeProfileDoesNotExist() {
         when(profileRepository.exists(feiProfile().getId())).thenReturn(false);
 
         profileExistsAssertion.run(feiProfile().getId());
     }
 
     @Test
-    public void shouldNotThrowExceptionIfMentorProfileExists() {
+    public void shouldNotThrowExceptionIfMenteeProfileExists() {
         when(profileRepository.exists(feiProfile().getId())).thenReturn(true);
 
         profileExistsAssertion.run(feiProfile().getId());
