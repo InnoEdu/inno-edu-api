e27bdcef09cb75c4793881821a817c67ae98a6bf
gustavodido <gustavo_di_domenico@gap.com>
2017-11-19 19:14:23 -0800

    Add the swagger configuration for availability.

--- src/main/java/inno/edu/api/controllers/AvailabilityController.java
+++ src/main/java/inno/edu/api/controllers/AvailabilityController.java
@@ -8,6 +8,10 @@ import inno.edu.api.domain.availability.commands.UpdateAvailabilityCommand;
 import inno.edu.api.domain.availability.models.Availability;
 import inno.edu.api.domain.availability.queries.GetAvailabilityByIdQuery;
 import inno.edu.api.domain.availability.repositories.AvailabilityRepository;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ResponseHeader;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -46,29 +50,49 @@ public class AvailabilityController {
     }
 
     @GetMapping
+    @ApiOperation(value = "Find all availability", notes = "Return all availability.", response = Availability.class, responseContainer = "List")
     public Resources<AvailabilityResource> all() {
         Iterable<Availability> availability = availabilityRepository.findAll();
         return resourceBuilder.from(availability, AvailabilityResource::new);
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Get an availability", notes = "Get an availability by ID.", response = Availability.class)
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Availability not found."),
+    })
     public AvailabilityResource get(@PathVariable UUID id) {
         return new AvailabilityResource(getAvailabilityByIdQuery.run(id));
     }
 
     @PostMapping
+    @ApiOperation(value = "Create a new availability", notes = "Creates a new availability.")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "New availability successfully created.", responseHeaders = @ResponseHeader(name = "Location", description = "Link to the new resource created.", response = String.class)),
+            @ApiResponse(code = 400, message = "Invalid mentor profile ID supplied."),
+    })
     public ResponseEntity<?> post(@RequestBody Availability availability) {
         AvailabilityResource availabilityResource = new AvailabilityResource(createAvailabilityCommand.run(availability));
         return availabilityResource.toCreated();
     }
 
     @PutMapping("/{id}")
+    @ApiOperation(value = "Update an availability", notes = "Update an availability.", response = Availability.class)
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "New availability successfully updated.", responseHeaders = @ResponseHeader(name = "Location", description = "Link to the updated resource.", response = String.class)),
+            @ApiResponse(code = 404, message = "Availability not found."),
+    })
     public ResponseEntity<?> put(@PathVariable UUID id, @RequestBody Availability availability) {
         AvailabilityResource availabilityResource = new AvailabilityResource(updateAvailabilityCommand.run(id, availability));
         return availabilityResource.toUpdated();
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "Delete an availability", notes = "Delete an availability, this operation cannot be undone.")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Availability successfully deleted."),
+            @ApiResponse(code = 404, message = "Availability not found.")
+    })
     public ResponseEntity<?> delete(@PathVariable UUID id) {
         deleteAvailabilityCommand.run(id);
         return noContent().build();
