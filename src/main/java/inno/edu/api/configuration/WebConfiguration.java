8d16fb7d4f1807d08d569b2d7edd2e997ccdd5e9
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-11-20 22:37:46 -0800

    Expose header to CORS.

--- src/main/java/inno/edu/api/configuration/WebConfiguration.java
+++ src/main/java/inno/edu/api/configuration/WebConfiguration.java
@@ -8,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .exposedHeaders("Access-Control-Allow-Origin");
     }
 }
