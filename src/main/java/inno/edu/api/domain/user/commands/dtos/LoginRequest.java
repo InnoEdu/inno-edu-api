3b6eb479cd7d7ef92bb985344d1efc658df8375d
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-11-26 15:42:53 -0800

    Add request validation for login.

--- src/main/java/inno/edu/api/domain/user/commands/dtos/LoginRequest.java
+++ src/main/java/inno/edu/api/domain/user/commands/dtos/LoginRequest.java
@@ -5,11 +5,19 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginRequest {
+    @NotNull
+    @Size(max = 30)
     private String username;
+
+    @NotNull
+    @Size(max = 30)
     private String password;
 }
