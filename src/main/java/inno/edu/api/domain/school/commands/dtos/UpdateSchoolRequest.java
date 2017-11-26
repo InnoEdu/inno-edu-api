dac4bc916ccab9982064f62d3bdf9af17f058776
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-11-26 15:50:30 -0800

    Add request validation for schools.

--- src/main/java/inno/edu/api/domain/school/commands/dtos/UpdateSchoolRequest.java
+++ src/main/java/inno/edu/api/domain/school/commands/dtos/UpdateSchoolRequest.java
@@ -5,12 +5,22 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 @Builder(toBuilder = true)
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateSchoolRequest {
+    @NotNull
+    @Size(max = 255)
     private String name;
+
+    @NotNull
     private String description;
+
+    @NotNull
+    @Size(max = 255)
     private String photoUrl;
 }
