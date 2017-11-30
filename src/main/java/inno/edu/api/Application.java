00a04afbadacc51210deb19694910c652e6d63b6
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-11-29 23:34:35 -0800

    Remove unused lines.

--- src/main/java/inno/edu/api/Application.java
+++ src/main/java/inno/edu/api/Application.java
@@ -1,15 +1,15 @@
 package inno.edu.api;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.TimeZone;
+
+import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-
-        SpringApplication.run(Application.class, args);
+        run(Application.class, args);
     }
 }
