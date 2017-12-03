cb980eb5f52977fadefabbb98e911df07f8c227e
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-12-03 15:52:41 -0800

    Add unit testing for avaialability by mentor.

--- src/test/java/inno/edu/api/domain/availability/queries/GetAvailabilityByMentorIdTest.java
+++ src/test/java/inno/edu/api/domain/availability/queries/GetAvailabilityByMentorIdTest.java
@@ -1,5 +1,41 @@
 package inno.edu.api.domain.availability.queries;
 
-public class GetAvailabilityByMentorIdTest {
+import inno.edu.api.domain.availability.models.Availability;
+import inno.edu.api.domain.availability.repositories.AvailabilityRepository;
+import inno.edu.api.domain.profile.queries.GetMentorActiveProfileByUserIdQuery;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.List;
+
+import static inno.edu.api.support.AvailabilityFactory.feiAvailability;
+import static inno.edu.api.support.ProfileFactory.feiProfile;
+import static inno.edu.api.support.UserFactory.fei;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GetAvailabilityByMentorIdTest {
+    @Mock
+    private AvailabilityRepository availabilityRepository;
+
+    @Mock
+    private GetMentorActiveProfileByUserIdQuery getMentorActiveProfileByUserIdQuery;
+
+    @InjectMocks
+    private GetAvailabilityByMentorId getAvailabilityByMentorId;
+
+    @Test
+    public void shouldGetAvailabilityByMentor() {
+        when(getMentorActiveProfileByUserIdQuery.run(fei().getId())).thenReturn(feiProfile());
+        when(availabilityRepository.findByMentorProfileId(feiProfile().getId())).thenReturn(feiAvailability());
+
+        List<Availability> availability = getAvailabilityByMentorId.run(fei().getId());
+
+        assertThat(availability, is(feiAvailability()));
+    }
 }
