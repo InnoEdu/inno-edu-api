3d3753dc48a3452d1c4e96143d329422cfb1c1d6
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-12-12 10:43:23 -0800

    Add unit test for user details service.

---
+++ src/test/java/inno/edu/api/infrastructure/security/service/UserDetailsServiceImplTest.java
@@ -0,0 +1,41 @@
+package inno.edu.api.infrastructure.security.service;
+
+import inno.edu.api.domain.user.repositories.UserRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import static inno.edu.api.support.UserFactory.fei;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserDetailsServiceImplTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Test(expected = UsernameNotFoundException.class)
+    public void shouldThrowExceptionIfUserNameNotFound() {
+        when(userRepository.findOneByUsername(fei().getUsername())).thenReturn(null);
+
+        userDetailsService.loadUserByUsername(fei().getUsername());
+    }
+
+    @Test
+    public void shouldReturnUserIfUsernameIsFound() {
+        when(userRepository.findOneByUsername(fei().getUsername())).thenReturn(fei());
+
+        UserDetails user = userDetailsService.loadUserByUsername(fei().getUsername());
+
+        assertThat(user.getUsername(), is(fei().getUsername()));
+        assertThat(user.getPassword(), is(fei().getPassword()));
+    }
+}
