9298ad5fd515542edb2db993b074a6bc6fd4e671
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-12-14 10:40:26 -0800

    Add static imports for constants.

--- src/main/java/inno/edu/api/infrastructure/security/service/TokenGeneratorService.java
+++ src/main/java/inno/edu/api/infrastructure/security/service/TokenGeneratorService.java
@@ -1,19 +1,21 @@
 package inno.edu.api.infrastructure.security.service;
 
-import inno.edu.api.infrastructure.security.jwt.SecurityConstants;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+
+import static inno.edu.api.infrastructure.security.jwt.SecurityConstants.EXPIRATION_TIME;
+import static inno.edu.api.infrastructure.security.jwt.SecurityConstants.SECRET;
+import static io.jsonwebtoken.SignatureAlgorithm.HS512;
 
 @Service
 public class TokenGeneratorService {
     public String generate(String username) {
         return Jwts.builder()
                 .setSubject(username)
-                .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS512, SecurityConstants.SECRET.getBytes())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(HS512, SECRET.getBytes())
                 .compact();
     }
 }
