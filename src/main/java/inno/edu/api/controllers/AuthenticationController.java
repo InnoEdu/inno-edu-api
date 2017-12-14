38cb307c4287dc0ff6eb60b3fa4f97bb18cf18d8
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-12-13 22:32:00 -0800

    Remove swagger from the application.

--- src/main/java/inno/edu/api/controllers/AuthenticationController.java
+++ src/main/java/inno/edu/api/controllers/AuthenticationController.java
@@ -3,13 +3,9 @@ package inno.edu.api.controllers;
 import inno.edu.api.controllers.resources.UserResource;
 import inno.edu.api.domain.user.commands.LoginCommand;
 import inno.edu.api.domain.user.commands.dtos.LoginRequest;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 public class AuthenticationController {
@@ -20,10 +16,6 @@ public class AuthenticationController {
     }
 
     @PostMapping("/login")
-    @ApiOperation(value = "Validate user credentials.", notes = "Validate the user name and user password supplied.")
-    @ApiResponses({
-            @ApiResponse(code = 400, message = "Invalid credentials supplied."),
-    })
     public UserResource login(@RequestBody LoginRequest request) {
         return new UserResource(loginCommand.run(request));
     }
