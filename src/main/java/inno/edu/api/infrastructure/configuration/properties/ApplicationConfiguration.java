5170302615568c0b0df9092429861ef815d55266
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-12-26 21:48:06 -0800

    Add metada configuration for properties.

--- src/main/java/inno/edu/api/infrastructure/configuration/properties/ApplicationConfiguration.java
+++ src/main/java/inno/edu/api/infrastructure/configuration/properties/ApplicationConfiguration.java
@@ -2,8 +2,10 @@ package inno.edu.api.infrastructure.configuration.properties;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+@EnableConfigurationProperties
 @ConfigurationProperties(prefix = "application")
 @Configuration
 @Data
