09f68d3f7160e97183151c53b5eefc02bf585840
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-01-08 23:24:39 -0800

    Add profile assertion to upload command.

--- src/main/java/inno/edu/api/domain/profile/root/commands/UploadProfileContentCommand.java
+++ src/main/java/inno/edu/api/domain/profile/root/commands/UploadProfileContentCommand.java
@@ -1,5 +1,6 @@
 package inno.edu.api.domain.profile.root.commands;
 
+import inno.edu.api.domain.profile.root.assertions.ProfileExistsAssertion;
 import inno.edu.api.infrastructure.annotations.Command;
 import inno.edu.api.infrastructure.storage.StorageService;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,13 +9,17 @@ import java.util.UUID;
 
 @Command
 public class UploadProfileContentCommand {
+    private final ProfileExistsAssertion profileExistsAssertion;
     private final StorageService storageService;
 
-    public UploadProfileContentCommand(StorageService storageService) {
+    public UploadProfileContentCommand(ProfileExistsAssertion profileExistsAssertion, StorageService storageService) {
+        this.profileExistsAssertion = profileExistsAssertion;
         this.storageService = storageService;
     }
 
     public void run(UUID id, MultipartFile file) {
+        profileExistsAssertion.run(id);
+
         storageService.save(id, file);
     }
 }
--- src/test/java/inno/edu/api/domain/profile/root/commands/UploadProfileContentCommandTest.java
+++ src/test/java/inno/edu/api/domain/profile/root/commands/UploadProfileContentCommandTest.java
@@ -1,5 +1,6 @@
 package inno.edu.api.domain.profile.root.commands;
 
+import inno.edu.api.domain.profile.root.assertions.ProfileExistsAssertion;
 import inno.edu.api.infrastructure.storage.StorageService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,6 +18,9 @@ public class UploadProfileContentCommandTest {
     private StorageService storageService;
 
     @Mock
+    private ProfileExistsAssertion profileExistsAssertion;
+
+    @Mock
     private MultipartFile multipartFile;
 
     @InjectMocks
@@ -27,5 +31,12 @@ public class UploadProfileContentCommandTest {
         uploadProfileContentCommand.run(feiProfile().getId(), multipartFile);
 
         verify(storageService).save(feiProfile().getId(), multipartFile);
+    }
+
+    @Test
+    public void shouldRunAllAssertions() {
+        uploadProfileContentCommand.run(feiProfile().getId(), multipartFile);
+
+        verify(profileExistsAssertion).run(feiProfile().getId());
     }
 }
