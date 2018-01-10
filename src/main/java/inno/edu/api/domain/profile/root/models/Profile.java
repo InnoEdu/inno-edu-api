0b10c68da5f72f6d27796453a401ee21d143dae3
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-01-09 23:10:06 -0800

    Add attachments to the profile resource.

--- src/main/java/inno/edu/api/domain/profile/root/models/Profile.java
+++ src/main/java/inno/edu/api/domain/profile/root/models/Profile.java
@@ -1,6 +1,7 @@
 package inno.edu.api.domain.profile.root.models;
 
 import inno.edu.api.domain.profile.accomplishment.models.Accomplishment;
+import inno.edu.api.domain.profile.attachment.models.ProfileAttachment;
 import inno.edu.api.domain.profile.experience.models.Experience;
 import inno.edu.api.domain.profile.interest.models.Interest;
 import inno.edu.api.domain.profile.service.models.Service;
@@ -52,4 +53,9 @@ public class Profile {
     @OneToMany(fetch = LAZY)
     @JoinColumn(name = "profileId", updatable = false, insertable = false)
     private List<Experience> experiences;
+
+    @OneToMany(fetch = LAZY)
+    @JoinColumn(name = "profileId", updatable = false, insertable = false)
+    private List<ProfileAttachment> attachments;
+
 }
