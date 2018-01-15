720ffaa627df5f71e2b52abc812226f72e970e90
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-01-14 21:06:33 -0800

    Add search appointment by status logic.

--- src/main/java/inno/edu/api/domain/appointment/root/queries/SearchAppointmentsQuery.java
+++ src/main/java/inno/edu/api/domain/appointment/root/queries/SearchAppointmentsQuery.java
@@ -1,10 +1,15 @@
 package inno.edu.api.domain.appointment.root.queries;
 
-import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import inno.edu.api.domain.appointment.root.models.Appointment;
 import inno.edu.api.domain.appointment.root.models.QAppointment;
 import inno.edu.api.domain.appointment.root.queries.dto.SearchAppointmentsRequest;
 import inno.edu.api.domain.appointment.root.repositories.AppointmentRepository;
 import inno.edu.api.infrastructure.annotations.Query;
+
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 @Query
 public class SearchAppointmentsQuery {
@@ -14,11 +19,15 @@ public class SearchAppointmentsQuery {
         this.appointmentRepository = appointmentRepository;
     }
 
-    public void run(SearchAppointmentsRequest request) {
+    public List<Appointment> run(SearchAppointmentsRequest request) {
         QAppointment query = QAppointment.appointment;
 
-        Predicate booleanExpression = query.status.in(request.getStatus());
+        BooleanExpression predicate = query.isNotNull();
 
-        appointmentRepository.findAll(booleanExpression);
+        if (request.getStatus() != null && !request.getStatus().isEmpty()) {
+            predicate = predicate.and(query.status.in(request.getStatus()));
+        }
+
+        return newArrayList(appointmentRepository.findAll(predicate));
     }
 }
---
+++ src/test/java/inno/edu/api/domain/appointment/root/queries/SearchAppointmentsQueryTest.java
@@ -0,0 +1,60 @@
+package inno.edu.api.domain.appointment.root.queries;
+
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import inno.edu.api.domain.appointment.root.models.QAppointment;
+import inno.edu.api.domain.appointment.root.queries.dto.SearchAppointmentsRequest;
+import inno.edu.api.domain.appointment.root.repositories.AppointmentRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static inno.edu.api.domain.appointment.root.models.AppointmentStatus.CANCELED;
+import static inno.edu.api.domain.appointment.root.models.AppointmentStatus.PROPOSED;
+import static inno.edu.api.support.AppointmentFactory.appointments;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SearchAppointmentsQueryTest {
+    @Mock
+    private AppointmentRepository appointmentRepository;
+
+    @InjectMocks
+    private SearchAppointmentsQuery searchAppointmentsQuery;
+
+    @Before
+    public void setUp() {
+        // The important part of the class is the predicate building, not the returned value
+        when(appointmentRepository.findAll(any(Predicate.class)))
+                .thenReturn(appointments());
+    }
+
+    @Test
+    public void shouldSearchByEmptyQuery() {
+        SearchAppointmentsRequest request = SearchAppointmentsRequest.builder().build();
+        searchAppointmentsQuery.run(request);
+
+        BooleanExpression expectedPredicate = QAppointment.appointment.isNotNull();
+        verify(appointmentRepository).findAll(expectedPredicate);
+    }
+
+    @Test
+    public void shouldSearchByStatus() {
+        SearchAppointmentsRequest request = SearchAppointmentsRequest.builder()
+                .status(newArrayList(PROPOSED, CANCELED))
+                .build();
+
+        searchAppointmentsQuery.run(request);
+
+        BooleanExpression expectedPredicate = QAppointment.appointment.isNotNull()
+                .and(QAppointment.appointment.status.in(newArrayList(PROPOSED, CANCELED)));
+
+        verify(appointmentRepository).findAll(expectedPredicate);
+    }
+}
