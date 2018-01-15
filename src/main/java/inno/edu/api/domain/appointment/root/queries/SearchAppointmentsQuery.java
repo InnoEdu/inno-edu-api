33daccd5b835862631acd6f7f7df91b5353d5913
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-01-14 19:38:40 -0800

    Add query to search appointments.

---
+++ src/main/java/inno/edu/api/domain/appointment/root/queries/SearchAppointmentsQuery.java
@@ -0,0 +1,10 @@
+package inno.edu.api.domain.appointment.root.queries;
+
+import inno.edu.api.infrastructure.annotations.Query;
+
+@Query
+public class SearchAppointmentsQuery {
+    public void run() {
+        
+    }
+}
