4e2f8354f8453a1b9ac89498ea2548e81cc6917f
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-01-21 19:39:18 -0800

    Add command to delete transactions.

---
+++ src/test/java/inno/edu/api/domain/user/transaction/commands/DeleteTransactionCommandTest.java
@@ -0,0 +1,10 @@
+package inno.edu.api.domain.user.transaction.commands;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by gdomenic on 21/01/18.
+ */
+public class DeleteTransactionCommandTest {
+
+}
