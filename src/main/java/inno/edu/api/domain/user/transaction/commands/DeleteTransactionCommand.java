2fc15f8ae5b0c69961a132a54f06dfd003b4e6b1
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-01-26 20:41:38 -0800

    Add business logic to delete transactions.

--- src/main/java/inno/edu/api/domain/user/transaction/commands/DeleteTransactionCommand.java
+++ src/main/java/inno/edu/api/domain/user/transaction/commands/DeleteTransactionCommand.java
@@ -1,4 +1,23 @@
 package inno.edu.api.domain.user.transaction.commands;
 
+import inno.edu.api.domain.user.transaction.assertions.TransactionExistsAssertion;
+import inno.edu.api.domain.user.transaction.repositories.TransactionRepository;
+import inno.edu.api.infrastructure.annotations.Command;
+
+import java.util.UUID;
+
+@Command
 public class DeleteTransactionCommand {
+    private final TransactionExistsAssertion transactionExistsAssertion;
+    private final TransactionRepository transactionRepository;
+
+    public DeleteTransactionCommand(TransactionExistsAssertion transactionExistsAssertion, TransactionRepository transactionRepository) {
+        this.transactionExistsAssertion = transactionExistsAssertion;
+        this.transactionRepository = transactionRepository;
+    }
+
+    public void run(UUID transactionId) {
+        transactionExistsAssertion.run(transactionId);
+        transactionRepository.delete(transactionId);
+    }
 }
--- src/test/java/inno/edu/api/domain/user/transaction/commands/DeleteTransactionCommandTest.java
+++ src/test/java/inno/edu/api/domain/user/transaction/commands/DeleteTransactionCommandTest.java
@@ -1,5 +1,39 @@
 package inno.edu.api.domain.user.transaction.commands;
 
+import inno.edu.api.domain.user.transaction.assertions.TransactionExistsAssertion;
+import inno.edu.api.domain.user.transaction.repositories.TransactionRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static inno.edu.api.support.UserFactory.feiTransaction;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
 public class DeleteTransactionCommandTest {
+    @Mock
+    private TransactionExistsAssertion transactionExistsAssertion;
+
+    @Mock
+    private TransactionRepository transactionRepository;
+
+    @InjectMocks
+    private DeleteTransactionCommand deleteTransactionCommand;
+
+    @Test
+    public void shouldCallRepositoryToDeleteTransaction() {
+        deleteTransactionCommand.run(feiTransaction().getId());
+
+        verify(transactionRepository).delete(feiTransaction().getId());
+    }
+
+    @Test
+    public void shouldRunAllAssertions() {
+        deleteTransactionCommand.run(feiTransaction().getId());
+
+        verify(transactionExistsAssertion).run(feiTransaction().getId());
+    }
 
 }
