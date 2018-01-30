4d251e314be2bed069c11044bebc64ebd63dc150
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-01-29 21:22:23 -0800

    Fix authentication filter encoding.

--- src/main/java/inno/edu/api/infrastructure/configuration/StaticConstants.java
+++ src/main/java/inno/edu/api/infrastructure/configuration/StaticConstants.java
@@ -4,6 +4,6 @@ public class StaticConstants {
     public static String NO_FILE = "";
     public static final String ERROR = "error";
     public static final String APPLICATION_JSON = "application/json";
-    public static final String UTF8 = "application/json";
+    public static final String UTF8 = "UTF-8";
     public static final String APPLICATION_HAL_JSON = "application/hal+json";
 }
